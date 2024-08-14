87e8d3aa63647a4fb31ea5362b99fae2f60c02b4
saaddighi <saaddighi20@gmail.com>
2024-08-14 14:26:36 +0000

    ok

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-import requests
 import json
+import requests
 
 response = requests.get('https://jsonplaceholder.typicode.com/todos')
 response2 = requests.get('https://jsonplaceholder.typicode.com/users')
