cf9c9cc489c0eb76e5207ea5ef3ff2ad8c9847f0
saaddighi <saaddighi20@gmail.com>
2024-09-18 14:12:11 +0000

    ok

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,39 @@
+
+"""
+Modules:
+    - json: Provides functions to parse JSON strings and convert
+    Python objects into JSON strings.
+    - requests: Allows sending HTTP/1.1 requests with
+    methods such as GET and POST.
+    - sys: Provides access to some variables used or
+    maintained by the Python interpreter, including `argv`.
+
+Usage:
+    The script can be run with command-line arguments passed via `sys.argv`.
+    It typically makes HTTP requests and processes JSON responses.
+"""
+
+import csv
+import json
+import requests
+import sys
+
+response = 'https://jsonplaceholder.typicode.com/todos'
+response2 = 'https://jsonplaceholder.typicode.com/users'
+
+if __name__ == "__main__":
+    url = requests.get(response2 ).json()
+    USER_ID = [i.get('id') for i in url]
+    for us in USER_ID:
+        user = requests.get(response2 + f'/{us}').json()
+        use = user.get('id')
+    
+    username = user.get('username')
+    todos = requests.get(response, params={"userId": USER_ID}).json()
+
+    data = {use: [{"task": t.get("title"), "completed":
+            t.get("completed"), "username": f"{username}"}for t in todos]}
+    
+
+    with open('todo_all_employees.json', 'w', newline='') as jsonfile:
+        json.dump(data, jsonfile)
