aa189a6731ac0b2f1fa2d88af724d2f69d1c27fb
saaddighi <saaddighi20@gmail.com>
2024-09-20 12:04:20 +0000

    ok

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Script to interact with the Reddit API using PRAW (Python Reddit API Wrapper).
+"""
+import praw
+
+if __name__ == "__main__":
+    def number_of_subscribers(subreddit):
+        reddit = praw.Reddit(
+            client_id = "eBcNZzIKJzH23dGoHND91w",
+            client_secret = "Ui3vaGH19po-U178PYWUROGrTJhGZw",
+            user_agent = "alx_project"
+        )
+        subreddit = reddit.subreddit(subreddit)
+        try:
+            subscriber_count = subreddit.subscribers
+        except:
+            subscriber_count = 0
+
+        return subscriber_count
