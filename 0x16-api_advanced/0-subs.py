829e756411ccef770cb7c79ddfab828b458a66bc
saaddighi <saaddighi20@gmail.com>
2024-09-23 13:18:18 +0000

    ok

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,48 +1,16 @@
-#!/usr/bin/python3
 
-"""
-Script to get the number of subscribers for a specific subreddit using PRAW (Python Reddit API Wrapper).
 
-Modules:
-    - praw: Python Reddit API Wrapper (PRAW) is a Python package that provides a simple interface to interact with Reddit's API.
+import requests
 
-Setup:
-    - Install PRAW using: `pip install praw`
-    - You must have a Reddit account and create an app to get the necessary credentials: client_id, client_secret, and user_agent.
-
-Functionality:
-    - The script defines a function `number_of_subscribers` that:
-        - Takes a subreddit name as input.
-        - Authenticates with Reddit's API using PRAW.
-        - Retrieves the number of subscribers for the given subreddit.
-        - Returns the number of subscribers, or 0 if the subreddit is not found or an error occurs.
-
-Authentication:
-    - client_id: Your Reddit app’s client ID.
-    - client_secret: Your Reddit app’s client secret.
-    - user_agent: A string identifying the app/script (e.g., "alx_project").
-
-How to Use:
-    1. Call the `number_of_subscribers(subreddit)` function with the subreddit name as an argument.
-    2. The function will return the number of subscribers for the specified subreddit.
-    3. Example:
-        subscriber_count = number_of_subscribers("learnpython")
-        print(f"Number of subscribers: {subscriber_count}")
-"""
-
-import praw
-
-if __name__ == "__main__":
-    def number_of_subscribers(subreddit):
-        reddit = praw.Reddit(
-            client_id = "eBcNZzIKJzH23dGoHND91w",
-            client_secret = "Ui3vaGH19po-U178PYWUROGrTJhGZw",
-            user_agent = "alx_project"
-        )
-        subreddit = reddit.subreddit(subreddit)
-        try:
-            subscriber_count = subreddit.subscribers
-        except:
-            subscriber_count = 0
-
-        return subscriber_count
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "alx_project"}
+    response = requests.get(url, headers, allow_redirects=False)
+    
+    if response.status_code == 200:
+        data = response.json()
+        subscriber_count = data["data"]["subscribers"]
+    else:
+        subscriber_count = 0
+        
+    return subscriber_count        
