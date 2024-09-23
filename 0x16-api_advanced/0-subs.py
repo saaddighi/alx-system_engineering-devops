05627219828af806748a0def61a088be99273cf8
saaddighi <saaddighi20@gmail.com>
2024-09-23 13:46:29 +0000

    ok

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,15 +3,16 @@
 
 import requests
 
+
 def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "alx_project"}
     response = requests.get(url, headers, allow_redirects=False)
-    
+
     if response.status_code == 200:
         data = response.json()
         subscriber_count = data["data"]["subscribers"]
     else:
-        subscriber_count = 0      
+        subscriber_count = 0
 
     return subscriber_count
