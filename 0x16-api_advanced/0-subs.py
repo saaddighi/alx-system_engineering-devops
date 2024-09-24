3cdb7f6e2fc71a744a85c6432847ea90e35fd70f
saaddighi <saaddighi20@gmail.com>
2024-09-24 13:09:53 +0000

    ok

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,12 @@
-#!/usr/bin/python3
+
 "requetsts module to make HTTP requests"
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "alx_project"}
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"   
+    headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers, allow_redirects=False)
 
     if response.status_code == 200:
